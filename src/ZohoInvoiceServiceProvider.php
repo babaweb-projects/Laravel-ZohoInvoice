ce67868b93114d3d6ca49b7a19924753ae507de2
Tetiana <krylova.tania@gmail.com>
2018-07-04 16:12:47 +0200

    v1.3

--- src/ZohoInvoiceServiceProvider.php
+++ src/ZohoInvoiceServiceProvider.php
@@ -13,7 +13,11 @@
 			}
 			$this->publishes([
           		__DIR__.'/../config/config.php' => config_path('zohoinvoice.php'),
-      		]);
+      		], 'config');
+
+      		$this->mergeConfigFrom(
+        		__DIR__.'/../config/zohoinvoice.php', 'zohoinvoice'
+    		);
 		}
 
 		public function register()
