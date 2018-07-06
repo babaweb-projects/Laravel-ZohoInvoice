0a26026bdf096483b5754fe9585851ed6c0de135
dev_guy <38211661+thomas-dev@users.noreply.github.com>
2018-07-06 14:24:44 +0200

    Update zohoinvoice.php

--- config/zohoinvoice.php
+++ config/zohoinvoice.php
@@ -2,7 +2,8 @@
 
 	return [
 		'authtoken' => env('ZOHO_AUTHTOKEN', ''),
-		'organization_id' => env('ZOHO_ORGANIZATION', '')
+		'organization_id' => env('ZOHO_ORGANIZATION', ''),
+		'baseurl' => env('ZOHO_BASEURL', 'https://invoice.zoho.eu/api/v3'),
 	];
 
 ?>
